d6dc5b91de92f3bf3f711a4335d523a89acda773
madhu179 <madhubabu232343@gmail.com>
2020-10-10 14:04:10 +0530

    Commiting Use Case 1 Files

---
+++ src/com/blz/hotelreservation/Hotel.java
@@ -0,0 +1,24 @@
+package com.blz.hotelreservation;
+
+public class Hotel {
+
+	private String hotelName;
+	private int hotelRateRegular;
+
+	public Hotel() {
+	}
+
+	public Hotel(String hotelName, int hotelRateRegular) {
+		this.hotelName = hotelName;
+		this.hotelRateRegular = hotelRateRegular;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public int getHotelRateRegularr() {
+		return hotelRateRegular;
+	}
+
+}
--- src/com/blz/hotelreservation/HotelReservationMain.java
+++ src/com/blz/hotelreservation/HotelReservationMain.java
@@ -1,10 +1,24 @@
 package com.blz.hotelreservation;
 
+import java.util.*;
+
 public class HotelReservationMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation System!");
+		Scanner scannerObject = new Scanner(System.in);
+		ArrayList<Hotel> hotelsArrayList = new ArrayList<Hotel>();
+		System.out.println("Enter the hotel details to add a hotel:");
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Enter Hotel Name :");
+			String hotelName = scannerObject.next();
 
+			System.out.println("Enter hotel's rate for regular customer");
+			int hotelRateRegular = scannerObject.nextInt();
+
+			Hotel hotel = new Hotel(hotelName, hotelRateRegular);
+			hotelsArrayList.add(hotel);
+		}
+		System.out.print("All hotel details addes successfully!");
 	}
 
 }
