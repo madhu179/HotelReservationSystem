147f2200029c625eaab9292c9febe0fd2eb1a107
madhu179 <madhubabu232343@gmail.com>
2020-10-10 13:56:26 +0530

    Commiting welcome program files

---
+++ src/com/blz/hotelreservation/HotelReservationMain.java
@@ -0,0 +1,10 @@
+package com.blz.hotelreservation;
+
+public class HotelReservationMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System!");
+
+	}
+
+}
